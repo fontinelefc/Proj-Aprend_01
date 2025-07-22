9e704cb68ac4bd5e8e4ed2f88c1b99cb8aacd4b2
fc_fontinele <filipe2cerqueira@gmail.com>
2025-07-21 23:18:18 -0300

    Aprimoramento de saída da impressão.

--- Digital_Organizer/organizer.py
+++ Digital_Organizer/organizer.py
@@ -2,6 +2,28 @@ import os
 
 
 def list_files_by_extension(folder_path):
+
+    arquivos = []
+    pastas = []
+
     items = os.listdir(folder_path)
     for name in items:
-        print(name)
+        full_path = os.path.join(folder_path, name)
+        if os.path.isfile(full_path):
+            arquivos.append(name)
+            arquivos.sort()
+        if os.path.isdir(full_path):
+            pastas.append(name)
+            pastas.sort()
+    
+    print('++++++++++','Arquivos:','++++++++++')
+    for file in arquivos:
+        full_path = os.path.join(folder_path, file)
+        size = os.path.getsize(full_path) / 1024  # Size in KB
+        print(f"{file} - {size:.2f} KB")
+
+    print('++++++++++','Pastas:','++++++++++')
+    for folder in pastas:
+        full_path = os.path.join(folder_path, folder)
+        size = os.path.getsize(full_path) / 1024  # Size in KB
+        print(f"{folder} - {size:.2f} KB")
