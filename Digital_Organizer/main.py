d0631a6ca8e0f81b276d0a5ab858a4e84ed3a474
fc_fontinele <filipe2cerqueira@gmail.com>
2025-07-22 17:29:49 -0300

    Refatoração do código e implementação de função print e size.

--- Digital_Organizer/main.py
+++ Digital_Organizer/main.py
@@ -1,3 +1,8 @@
-from organizer import list_files_by_extension
+import os
 
-list_files_by_extension("tests")
+from organizer import list_files_by_extension, print_formatted_lists
+
+if __name__ == "__main__":
+    caminho = input("Digite o caminho da pasta: ")
+    arquivos, pastas = list_files_by_extension(caminho)
+    print_formatted_lists(arquivos, pastas)
--- Digital_Organizer/organizer.py
+++ Digital_Organizer/organizer.py
@@ -1,5 +1,14 @@
 import os
 
+def get_folder_size(folder_path):
+    total_size = 0
+    for item in os.listdir(folder_path):
+        full_path = os.path.join(folder_path, item)
+        if os.path.isfile(full_path):
+            total_size += os.path.getsize(full_path)
+        elif os.path.isdir(full_path):
+            total_size += get_folder_size(full_path)
+    return total_size / (1024*1024) # Size in MB
 
 def list_files_by_extension(folder_path):
 
@@ -10,20 +19,21 @@ def list_files_by_extension(folder_path):
     for name in items:
         full_path = os.path.join(folder_path, name)
         if os.path.isfile(full_path):
-            arquivos.append(name)
-            arquivos.sort()
-        if os.path.isdir(full_path):
-            pastas.append(name)
-            pastas.sort()
-    
-    print('++++++++++','Arquivos:','++++++++++')
-    for file in arquivos:
-        full_path = os.path.join(folder_path, file)
-        size = os.path.getsize(full_path) / 1024  # Size in KB
-        print(f"{file} - {size:.2f} KB")
+            size_mb = os.path.getsize(full_path) / (1024*1024)
+            arquivos.append((name, size_mb))
+        elif os.path.isdir(full_path):
+            size_mb = get_folder_size(full_path)
+            pastas.append((name, size_mb))
 
-    print('++++++++++','Pastas:','++++++++++')
-    for folder in pastas:
-        full_path = os.path.join(folder_path, folder)
-        size = os.path.getsize(full_path) / 1024  # Size in KB
-        print(f"{folder} - {size:.2f} KB")
+    arquivos.sort(key=lambda x: x[0].lower())
+    pastas.sort(key=lambda x: x[0].lower())
+    return arquivos, pastas
+
+def print_formatted_lists(arquivos, pastas):
+    print("++++++++++ Arquivos ++++++++++")
+    for name, total_size in arquivos:
+        print(f"{name} - {total_size:.2f} MB")
+
+    print("++++++++++ Pastas ++++++++++")
+    for name, total_size in pastas:
+        print(f"{name} - {total_size:.2f} MB")
